1867dfdaf42f91578381581c631298098cba4de9
hakanonymos <30985149+hakanonymos@users.noreply.github.com>
2020-09-12 06:59:18 +0100

    Add files via upload

--- panel/config.php
+++ panel/config.php
@@ -3,7 +3,7 @@
 
 //These are for accessing your web-panel. You can change these any time.
 
-$panelusername = "admin2";  //set your USER
+$panelusername = "admin";  //set your USER
 $panelpassword = "admin"; // set your password
 
 
